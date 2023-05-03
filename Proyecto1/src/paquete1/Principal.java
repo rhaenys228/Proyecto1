a028821382f894a38817dd71759290be73d2ee9f
rhaen <rhaen@DESKTOP-7F4RE5F>
2023-05-03 09:35:34 +0200

    Este es el tercer commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -1,10 +1,12 @@
 package paquete1;
 
 public class Principal {
-
+	// https://github.com/rhaenys228/Proyecto1.git 
+	// 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 |token
 	public static void main(String[] args) {
 		System.out.println("Ejemplo de control de versiones con Git");
 		System.out.println("Esto es un cambio");
+		System.out.println("Esto es otro cambio");
 	}
 
 }
